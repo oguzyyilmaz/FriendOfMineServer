9c1ff92bd9c9e8ea079d6d9b9ea6f5d74212b72a
Oğuzhan YILMAZ <yilmazoguz94@gmail.com>
2017-07-08 10:37:58 +0300

    config.java update

--- src/main/java/com/msesoft/fom/config/Config.java
+++ src/main/java/com/msesoft/fom/config/Config.java
@@ -4,5 +4,5 @@ package com.msesoft.fom.config;
  * Created by oguz on 9/8/16.
  */
 public interface Config {
-    static final String ROOT_URL = "http://192.168.2.5";
+    static final String ROOT_URL = "/*server-url*/";
 }
